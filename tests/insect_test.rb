bab91e3538459b258c04c4f0cb712eb863d975b4
Andrew Ek <andrewek@gmail.com>
2016-03-28 10:00:22 -0500

    Better tests

--- tests/insect_test.rb
+++ tests/insect_test.rb
@@ -9,4 +9,22 @@ class InsectTest < Minitest::Test
     assert_equal("Fuzzy Mantis", i.name)
   end
 
+  def test_location_accessors
+    i = Insect.new(name: "Mantis", location: "Tree", seen_by: "Andrew", description: "Fierce")
+    i.location = "A scary cave"
+    assert_equal("A scary cave", i.location)
+  end
+
+  def test_seen_by_accessors
+    i = Insect.new(name: "Mantis", location: "Tree", seen_by: "Andrew", description: "Fierce")
+    i.seen_by = "Ruth"
+    assert_equal("Ruth", i.seen_by)
+  end
+
+  def test_description_accessors
+    i = Insect.new(name: "Mantis", location: "Tree", seen_by: "Andrew", description: "Fierce")
+    i.description = "Cuddly"
+    assert_equal("Cuddly", i.description)
+  end
+
 end
